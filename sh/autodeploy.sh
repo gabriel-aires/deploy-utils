fb58ab2421bc6cee068f770f28a96fc84d85a50d
root <root@cetaskspdin01.anatel.gov.br>
2015-03-19 17:15:32 -0300

    script de deploy automático finalizado

--- sh/autodeploy.sh
+++ sh/autodeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pid=$$
+
 ##### Execução somente como usuário root ######
 
 if [ ! "$USER" == 'root' ]; then
@@ -10,18 +12,30 @@ fi
 #### Inicialização #####
 
 deploy_dir="/opt/autodeploy-paginas"									#diretório de instalação.
-source $deploy_dir/conf/global.conf || exit	1							#carrega o arquivo de constantes.
+source $deploy_dir/conf/global.conf || exit 1								#carrega o arquivo de constantes.
 
-#### Cria lockfile #########
+temp_dir="$temp/$pid"
 
-if [ -d $lock_dir ]; then
-	if [ -f $lock_dir/autodeploy ]; then
-		echo -e "O script de deploy automático já está em execução." && exit 0
-	else
-		touch $lock_dir/autodeploy 
-	fi
+if [ -z "$regex_temp_dir" ] \
+	|| [ -z "$regex_lock_dir" ] \
+	|| [ -z $(echo $temp_dir | grep -E "$regex_temp_dir") ] \
+	|| [ -z $(echo $lock_dir | grep -E "$regex_lock_dir") ] \
+	|| [ ! -d "$temp" ] \
+	|| [ ! -d "$lock_dir" ] \
+	|| [ ! -d "$parametros_app" ] \
+	|| [ -z "$ambientes" ];
+then
+	echo 'Favor preencher corretamente o arquivo global.conf e tentar novamente.'
+	exit 1
+fi
+
+#### Cria lockfile e diretório temporário #########
+
+if [ -f $lock_dir/autodeploy ]; then
+	echo -e "O script de deploy automático já está em execução." && exit 0
 else
-	echo "A variável lock_dir deve ser preenchida no arquivo global.conf" && exit 1
+	touch $lock_dir/autodeploy
+	mkdir -p $temp_dir
 fi
 
 #### Renovação do ticket kerberos ########
@@ -30,30 +44,27 @@ kinit -R || exit 1
 
 #### Deploy em todos os ambientes ########
 
-if [ ! -z "$ambientes" ]; then
-    lista_ambientes=$(echo "$ambientes" | sed -r 's/,/ /g' | sed -r 's/;/ /g' | sed -r 's/ +/ /g' | sed -r 's/ $//g' | sed -r 's/^ //g')
-else
-    echo "A variável ambientes deve ser preenchida no arquivo global.conf" && exit 1
-fi
+echo "$ambientes" | sed -r 's/,/ /g' | sed -r 's/;/ /g' | sed -r 's/ +/ /g' | sed -r 's/ $//g' | sed -r 's/^ //g' | sed -r 's/ /\n/g'> $temp_dir/lista_ambientes
 
-if [ -d "$parametros_app"; then
-    while read -d ' ' ambiente; do
-        lista_aplicacoes=$(grep -REl "^auto_desenvolvimento='1'$" $parametros_app | sed -r "s|$parametros_app/(.+)\.conf|\1|g")
-        if [ ! -z "$lista_aplicacoes" ];then
-            while read -d ' ' aplicacao; do
-                $deploy_dir/deploy_paginas.sh -f $aplicacao auto $ambiente
-                wait
-            done < $lista_aplicacoes
-        else
-            echo "O deploy automático não foi habilitado no ambiente '$ambiente'"
-        fi
-    done < $lista_ambientes
-else
-    echo "Não foi encontrado o diretório que contém os parâmetros de deploy das aplicações." && exit 1
-fi
+while read ambiente; do
+	grep -REl "^auto_$ambiente='1'$" $parametros_app > $temp_dir/lista_aplicacoes
+	sed -i -r "s|^$parametros_app/(.+)\.conf$|\1|g" $temp_dir/lista_aplicacoes
 
-##### Remove lockfile #####
+	if [ ! -z "$(cat $temp_dir/lista_aplicacoes)" ];then
+        	while read aplicacao; do
+			$deploy_dir/sh/deploy_paginas.sh -f $aplicacao auto $ambiente
+			wait
+			echo -e "\n------------------------------------------------------\n"
+		done < "$temp_dir/lista_aplicacoes"
+	else
+		echo "O deploy automático não foi habilitado no ambiente '$ambiente'"
+		echo -e "\n------------------------------------------------------\n"
+	fi
+done < "$temp_dir/lista_ambientes"
 
-rm -f $lock_dir/autodeploy
+##### Remove lockfile e diretório temporário #####
 
+rm -f $temp_dir/*
+rmdir $temp_dir
+rm -f "$lock_dir/autodeploy"
 exit 0
