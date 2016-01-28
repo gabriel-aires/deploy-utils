7043b9527679322b752050211ed4215d3bb459e7
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-01-28 08:39:41 -0200

    atualizada a função query_file

--- src/common/sh/include.sh
+++ src/common/sh/include.sh
@@ -328,44 +328,151 @@ function set_app_history_dirs () {
 
 function query_file () {
 
-    local file="$1"
-    local delim="$(echo "$2" | sed -r "s|([\\\+\-\.\?\^\$])|\\\\\1|g")"
-    local selection="$(echo "\\$3$delim" | sed -r "s| |$delim\\\\|g")"
-    local filter="$4"
-    local value="$5"
+    local file=''
+    local delim=''
+    local output_delim='\t'
+    local columns=()
+    local set_value=()
+    local s_index=0
+    local filter=()
+    local filter_type=()
+    local filter_value=()
+    local f_index=0
+    local grep_cmd="grep -E -x"
+    local selection=''
+    local where=''
 
-    if [ ! -f "$file" ]; then
-        echo "$1: Arquivo inexistente." 1>&2; return 1
+    while true; do
+        case "$1" in
+
+            "-d"|"--delim")
+                delim="$(echo "$2" | sed -r "s|([\\\+\-\.\?\^\$])|\\\\\1|g")"
+                shift 2
+                ;;
+
+            "-r"|"--replace-delim")
+                output_delim="$(echo "$2" | sed -r "s|([\\\+\-\.\?\^\$])|\\\\\1|g")"
+                shift 2
+                ;;
+
+            "-s"|"--select")
+                while echo "$2" | grep -Ex "[0-9]+"; do
+                    columns[$s]="$2"
+                    ((s_index++))
+                    shift
+                done
+                shift
+                ;;
+
+            "-f"|"--from")
+                file="$2"
+                shift 2
+                ;;
+
+            "-w"|"--where")
+                while echo "$2" | grep -Ex "[0-9]+(==|=\~|=%|\!=).*)?"; do
+                    filter[$f]="$(echo "$2" | sed -r 's|^([0-9]+).*$|\1|')"
+                    filter_type[$f]="$(echo "$2" | sed -r 's|^[0-9]+(==|=\~|=%|\!=).*$|\1|')"
+                    filter_value[$f]="$(echo "$2" | sed -r 's|^[0-9]+(==|=\~|=%|\!=)(.*)$|\2|')"
+                    ((f_index++))
+                    shift
+                done
+                shift
+                ;;
+
+            "-h"|"--help")
+                echo "Utilização: query_file [nomearquivo] [opções]"
+                echo "Opções:"
+                echo "-d|--delim: especificar caractere ou string que delimita os campos do arquivo. Ex: ';' (obrigatório)"
+                echo "-r|--replace-delim: especificar caractere ou string que delimitará os campos exibidos. Ex: '|' (opcional)"
+                echo "-s|--select: especificar ordem das colunas a serem selecionadas. Ex: "1" "2", etc (obrigatório)"
+                echo "-f|--from: especificar arquivo. Ex: dados.csv (obrigatório)"
+                echo "-w|--where: especificar filtro. Ex: '1==valor_exato' '2=~regex_valor' '3!=diferente_valor' '4=%contem_valor', etc (opcional)"
+                return 0
+                ;;
+
+            '')
+                break
+                ;;
+
+            *)
+                echo "'$1':Argumento inválido." 1>&2
+                return 1
+                ;;
+
+        esac
+    done
+
+    if [ ! -f "$file" ] || [ -z "$delim" ] || [ -z "${columns[0]}"]; then
+        echo "Erro. Argumentos insuficientes." 1>&2; return 1
     elif ! echo "$delim" | grep -Ex "[[:print:]]+" > /dev/null; then
-        echo "$2: Delimitador inválido." 1>&2; return 1
-    elif ! echo "$selection" | grep -Ex "(\\\\[0-9]+$delim)*\\\\[0-9]+$delim" > /dev/null; then
-        echo "$3: Seleção inválida." 1>&2; return 1
-    elif [ "$filter" -lt 1 ]; then
-        echo "$4: Filtro inválido." 1>&2; return 1
-    elif ! echo "$value" | grep -Ex "[[:print:]]+" | grep -Ev "$delim" > /dev/null; then
-        echo "$5: Valor inválido." 1>&2; return 1
+        echo "Delimitador inválido." 1>&2; return 1
     fi
 
+    local header="$(head -n 1 $file )"
     local size=1
     local part_regex="(.*)$delim"
     local line_regex=''
-    local filter_regex=''
+    local filter_regex=()
+    local filter_cmd=()
+    local index
 
-    while $(grep -El "^(.*$delim){$size}" $file > /dev/null); do
+    while $(echo "$header" | grep -E "^(.*$delim){$size}" > /dev/null); do
 
         line_regex="$line_regex$part_regex"
+        index=0
 
-        if [ $size -eq $filter ]; then
-            filter_regex="$filter_regex$value$delim"
-        else
-            filter_regex="$filter_regex$part_regex"
-        fi
+        while [ $index -lt $f_index ]; do
+
+            if [ $size -eq $filter[$index] ]; then
+
+                case $filter_type[$index] in
+                    '==') # match exato
+                        filter_value[$index]=$(echo "$filter_value[$index]" | sed -r "s|([\\\+\-\.\?\^\$])|\\\\\1|g")
+                        filter_cmd[$index]="$grep_cmd"
+                        ;;
+                    '=%') # contains
+                        filter_value[$index]=$(echo "$filter_value[$index]" | sed -r "s|([\\\+\-\.\?\^\$])|\\\\\1|g" | sed -r "s|^(.*)$|\.\*\1\.\*|")
+                        filter_cmd[$index]="$grep_cmd"
+                        ;;
+                    '=~') # regex
+                        filter_cmd[$index]="$grep_cmd"
+                        ;;
+                    '!=') # match inverso
+                        filter_value[$index]=$(echo "$filter_value[$index]" | sed -r "s|([\\\+\-\.\?\^\$])|\\\\\1|g")
+                        filter_cmd[$index]="$grep_cmd -v"
+                        ;;
+                esac
+
+                filter_regex[$index]="$filter_regex[$index]$filter_value[$index]$delim"
+
+            else
+
+                filter_regex[$index]="$filter_regex[$index]$part_regex"
+
+            fi
+
+            ((index++))
+
+        done
 
         ((size++))
 
     done
 
-    test -n "$filter_regex" && grep -Ex "$filter_regex" $file | sed -r "s|$line_regex|$selection|" || return 1
+    index=0
+    while [ $index -lt $f_index ]; do
+        where="$where $filter_cmd[$index] $filter_regex[$index] |"
+        ((index++))
+    done
+
+    index=0
+    while [ $index -lt $s_index ]; do
+        selection="$selection\\$columns[$index]$output_delim"
+        ((index++))
+    done
+
+    cat $file | $where sed -r "s|$line_regex|$selection|" || return 1
 
     return 0
 
