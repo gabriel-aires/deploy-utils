d3da32631f82608752cebd5d1285d81779820945
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-03-08 17:23:18 -0300

    revisão do script de setup; adicionado suporte a mod_ssl

--- src/server/sh/setup.sh
+++ src/server/sh/setup.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
+function end() {
+
+    if [ "$1" != '0' ]; then
+        echo "ERRO. Instalação interrompida."
+    fi
+
+    exit $1
+
+}
+
 case "$1" in
-    --reconfigure) $(dirname $(dirname $(dirname $(readlink -f $0))))/common/sh/reconfigure.sh && echo "Reconfigurando serviço..." || exit 1 ;;
+    --reconfigure) $(dirname $(dirname $(dirname $(readlink -f $0))))/common/sh/reconfigure.sh && echo "Reconfigurando serviço..." || end 1 ;;
     '') echo "Configurando serviço..." ;;
-    *)  echo "Argumento inválido" && exit 1 ;;
+    *)  echo "Argumento inválido" && end 1 ;;
 esac
 
-source $(dirname $(dirname $(dirname $(readlink -f $0))))/common/sh/include.sh || exit 1
-source $install_dir/sh/include.sh || exit 1
+source $(dirname $(dirname $(dirname $(readlink -f $0))))/common/sh/include.sh || end 1
+source $install_dir/sh/include.sh || end 1
+
+valid "ssl_enable" "regex_bool" "\nErro. A variável ssl_enable é booleana (true/false)."
+valid "set_apache_listen_directive" "regex_bool" "\nErro. A variável set_apache_listen_directive é booleana (true/false)."
+valid "set_apache_namevirtualhost_directive" "regex_bool" "\nErro. A variável set_apache_namevirtualhost_directive é booleana (true/false)."
 
 #verify apache params
-test -d $apache_confd_dir || exit 1
-test -x $apache_init_script || exit 1
-id $apache_user > /dev/null || exit 1
-groups $apache_user | sed -r "s|$apache_user :||" | grep " $apache_group" > /dev/null || exit 1
+test -d $apache_confd_dir || end 1
+test -z $apache_init_script || end 1
+id $apache_user > /dev/null || end 1
+groups $apache_user | sed -r "s|$apache_user :||" | grep " $apache_group" > /dev/null || end 1
 
 #backup vhost_conf
 test -f $apache_confd_dir/$apache_vhost_filename && cp -f $apache_confd_dir/$apache_vhost_filename $apache_confd_dir/$apache_vhost_filename.bak
 
 #setup vhost_conf
-cp -f $install_dir/template/vhost.template $apache_confd_dir/$apache_vhost_filename || exit 1
+$ssl_enable && vhost_template=$install_dir/template/vhost_ssl.template || vhost_template=$install_dir/template/vhost.template
+$set_apache_namevirtualhost_directive && apache_namevirtualhost_directive="NameVirtualHost $apache_vhost_name:$apache_vhost_port" || apache_namevirtualhost_directive=''
+$set_apache_listen_directive && apache_listen_directive="Listen $apache_vhost_port" || apache_listen_directive=''
 
+cp -f $vhost_template $apache_confd_dir/$apache_vhost_filename || end 1
+test -w $vhost_template $apache_confd_dir/$apache_vhost_filename || end 1
+
+sed -i -r "s|@ssl_crt_path|$ssl_crt_path|" $apache_confd_dir/$apache_vhost_filename
+sed -i -r "s|@ssl_key_path|$ssl_key_path|" $apache_confd_dir/$apache_vhost_filename
 sed -i -r "s|@apache_namevirtualhost_directive|$apache_namevirtualhost_directive|" $apache_confd_dir/$apache_vhost_filename
 sed -i -r "s|@apache_listen_directive|$apache_listen_directive|" $apache_confd_dir/$apache_vhost_filename
 sed -i -r "s|@apache_vhost_name|$apache_vhost_name|" $apache_confd_dir/$apache_vhost_filename
@@ -40,55 +61,56 @@ sed -i -r "s|@apache_vhost_logname|$apache_vhost_logname|" $apache_confd_dir/$ap
 test -f $service_init_script && cp -f $service_init_script $service_init_script.bak
 
 #setup deploy_service
-cp -f $install_dir/template/service.template $service_init_script || exit 1
+cp -f $install_dir/template/service.template $service_init_script || end 1
+test -w $service_init_script || end 1
 
 sed -i -r "s|@src_dir|$src_dir|" $service_init_script
 sed -i -r "s|@daemon_log|$history_dir/$service_log_file|" $service_init_script
 
 #create directories
-mkdir -p $common_work_dir || exit 1
-mkdir -p $common_log_dir || exit 1
-mkdir -p $history_dir || exit 1
-mkdir -p $app_conf_dir || exit 1
-mkdir -p $work_dir || exit 1
-mkdir -p $log_dir || exit 1
-mkdir -p $lock_dir || exit 1
+mkdir -p $common_work_dir || end 1
+mkdir -p $common_log_dir || end 1
+mkdir -p $history_dir || end 1
+mkdir -p $app_conf_dir || end 1
+mkdir -p $work_dir || end 1
+mkdir -p $log_dir || end 1
+mkdir -p $lock_dir || end 1
 
 #create deploy_queue
 if [ ! -p "$deploy_queue" ]; then
-    mkfifo "$deploy_queue" || exit 1
+    mkfifo "$deploy_queue" || end 1
 fi
 
 #setup owner/permissions
-chmod 775 $common_work_dir || exit 1
-chmod 775 $common_log_dir || exit 1
-chmod 755 $src_dir/common/sh/query_file.sh || exit 1
-chmod 755 $service_init_script || exit 1
-chmod 770 $deploy_queue || exit 1
-chmod 775 $history_dir || exit 1
-chmod 775 $app_conf_dir || exit 1
-chmod 775 $work_dir || exit 1
-chmod 775 $log_dir || exit 1
-chmod 775 $lock_dir || exit 1
-chmod 755 $src_dir/server/cgi/* || exit 1
-chmod 644 $src_dir/server/css/* || exit 1
+chmod 775 $common_work_dir || end 1
+chmod 775 $common_log_dir || end 1
+chmod 755 $src_dir/common/sh/query_file.sh || end 1
+chmod 755 $service_init_script || end 1
+chmod 770 $deploy_queue || end 1
+chmod 775 $history_dir || end 1
+chmod 775 $app_conf_dir || end 1
+chmod 775 $work_dir || end 1
+chmod 775 $log_dir || end 1
+chmod 775 $lock_dir || end 1
+chmod 755 $src_dir/server/cgi/* || end 1
+chmod 644 $src_dir/server/css/* || end 1
 
-chgrp $apache_group $common_work_dir || exit 1
-chgrp $apache_group $common_log_dir || exit 1
-chgrp $apache_group $src_dir/common/sh/query_file.sh || exit 1
-chgrp $apache_group $service_init_script || exit 1
-chgrp $apache_group $deploy_queue
-chgrp -R $apache_group $history_dir || exit 1
-chgrp -R $apache_group $app_conf_dir || exit 1
-chgrp -R $apache_group $work_dir || exit 1
-chgrp -R $apache_group $log_dir || exit 1
-chgrp -R $apache_group $lock_dir || exit 1
-chgrp -R $apache_group $src_dir/server/cgi || exit 1
-chgrp -R $apache_group $src_dir/server/css || exit 1
+chgrp $apache_group $common_work_dir || end 1
+chgrp $apache_group $common_log_dir || end 1
+chgrp $apache_group $src_dir/common/sh/query_file.sh || end 1
+chgrp $apache_group $service_init_script || end 1
+chgrp $apache_group $deploy_queue || end 1
+chgrp -R $apache_group $history_dir || end 1
+chgrp -R $apache_group $app_conf_dir || end 1
+chgrp -R $apache_group $work_dir || end 1
+chgrp -R $apache_group $log_dir || end 1
+chgrp -R $apache_group $lock_dir || end 1
+chgrp -R $apache_group $src_dir/server/cgi || end 1
+chgrp -R $apache_group $src_dir/server/css || end 1
 
 #restart services
-$apache_init_script restart || exit 1
-$service_init_script restart || exit 1
+$apache_init_script restart || end 1
+$service_init_script restart || end 1
 
 echo "Instalação concluída."
-exit 0
+end 0
