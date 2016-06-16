e1a6b05555e51439b4e3b7abe07f09c395a0a31d
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-06-16 14:45:43 -0300

    adicionada mensagem de log

--- src/agents/sh/apache_2.sh
+++ src/agents/sh/apache_2.sh
@@ -35,6 +35,7 @@ function copy_log () {
         ls $logpath* | while read logfile; do
             test ! -f "$logfile" && log "ERRO" "'$logfile' não é um arquivo. Continuando..." && continue
             test "$(file -bi "$logfile" | cut -d / -f1)" != 'text' && log "INFO" "'$logfile' não é um arquivo de texto. Continuando..." && continue
+            log "INFO" "Adicionando o arquivo $logfile..."
             grep -F "$app" "$logfile" > "$zippipe" & zip --fifo -q -l -1 "$zipfile" "$zippipe"
         done
 
