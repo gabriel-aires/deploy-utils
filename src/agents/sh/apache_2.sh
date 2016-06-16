bee6b982ea9ae57f50cc0978e77134718cd8cc43
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-06-15 22:16:43 -0300

    bugfix

--- src/agents/sh/apache_2.sh
+++ src/agents/sh/apache_2.sh
@@ -11,7 +11,9 @@ function copy_log () {
     test ! -f "$apache_configuration" && log "ERRO" "Arquivo de configuração do apache não identificado. Abortando..." && return 1
     serverroot="$(grep -Ex "$regex_serverroot_directive" "$apache_configuration" | tail -n 1 | sed -r "s|^[[:blank:]]*||" | cut -d ' ' -f2 | sed -r 's|\"||g' | sed -r "s|\'||g")"
     test ! -d "$serverroot" && log "ERRO" "Diretório raiz do apache não identificado. Abortando..." && return 1
-    cd $serverroot &> /dev/null
+
+    working_dir="$(pwd)"
+    cd $serverroot
 
     grep -Ex "$regex_include_directive" "$apache_configuration" | sed -r "s|^[[:blank:]]*||" | cut -d ' ' -f2 | sed -r 's|\"||g' | sed -r "s|\'||g" > $tmp_dir/apache_includes.list
     grep -Ex "$regex_log_directive" "$apache_configuration" | sed -r "s|^[[:blank:]]*||" | cut -d ' ' -f2 | sed -r 's|\"||g' | sed -r "s|\'||g" > $tmp_dir/apache_logs.list
@@ -24,12 +26,12 @@ function copy_log () {
         test -f "$logfile" || continue
         zipfile="${shared_log_dir}/$(basename $logfile).zip"
         log "INFO" "Criando o arquivo $zipfile..."
-        zip -ql1 "$zipfile" "$logfile"
+        cd "$(dirname $logfile)" ; zip -ql1 "$zipfile" "$(basename $logfile)" ; cd - &> /dev/null
     done
 
     log "INFO" "Fim da transferência de logs."
+    cd "$working_dir"
 
-    cd - &> /dev/null
     return 0
 
 }
