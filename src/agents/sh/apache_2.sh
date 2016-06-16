8eff00f824b18bcf78312fade76f8586b5b426c5
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-06-15 21:44:31 -0300

    bugfix

--- src/agents/sh/apache_2.sh
+++ src/agents/sh/apache_2.sh
@@ -11,6 +11,7 @@ function copy_log () {
     test ! -f "$apache_configuration" && log "ERRO" "Arquivo de configuração do apache não identificado. Abortando..." && return 1
     serverroot="$(grep -Ex "$regex_serverroot_directive" "$apache_configuration" | tail -n 1 | sed -r "s|^[[:blank:]]*||" | cut -d ' ' -f2 | sed -r 's|\"||g' | sed -r "s|\'||g")"
     test ! -d "$serverroot" && log "ERRO" "Diretório raiz do apache não identificado. Abortando..." && return 1
+    cd $serverroot &> /dev/null
 
     grep -Ex "$regex_include_directive" "$apache_configuration" | sed -r "s|^[[:blank:]]*||" | cut -d ' ' -f2 | sed -r 's|\"||g' | sed -r "s|\'||g" > $tmp_dir/apache_includes.list
     touch $tmp_dir/apache_logs.list
@@ -23,11 +24,12 @@ function copy_log () {
         test -f "$logfile" || continue
         zipfile="${shared_log_dir}/$(basename $logfile).zip"
         log "INFO" "Criando o arquivo $zipfile a partir de $logfile..."
-        cd $serverroot; zip -ql1 ${shared_log_dir}/${instancia_jboss}.zip $logfile; cd - > /dev/null
+        zip -ql1 "$zipfile" "$logfile"
     done
 
     log "INFO" "Fim da transferência de logs."
 
+    cd - &> /dev/null
     return 0
 
 }
