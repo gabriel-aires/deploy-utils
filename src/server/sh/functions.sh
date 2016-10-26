1255f412dd4094f78ba6ee36db515ac6a0ccd1a0
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-10-26 14:17:40 -0200

    bugfix

--- src/server/sh/functions.sh
+++ src/server/sh/functions.sh
@@ -189,13 +189,11 @@ function web_links () {
                 echo "<div class=\"dropdown\">"
                 echo "  <div class=\"header_button\">$category_title &or;</div>"
                 echo "  <div class=\"dropdown_content\">"
-                echo "      <ul>"
                 cat $tmp_dir/cgi_${category}_pages | while read link_name; do
                     link_uri="$(dirname $SCRIPT_NAME)/$link_name.cgi"
                     link_title="$(eval "echo \$cgi_${link_name}_title")"
-                    echo "          <li><a href=\"$link_uri\">$link_title</a></li>"
+                    echo "      <a href=\"$link_uri\">$link_title</a>"
                 done
-                echo "      </ul>"
                 echo "  </div>"
                 echo "</div>"
                 ;;
