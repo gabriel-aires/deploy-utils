4a65665207d54124458274fc0eaee34219176a86
Gabriel Aires Guedes <airesgabriel@anatel.gov.br>
2016-10-26 16:25:27 -0200

    bugfix

--- src/server/sh/functions.sh
+++ src/server/sh/functions.sh
@@ -332,9 +332,7 @@ function web_footer () {
     echo "              $nav_right"
     echo "          </div>"
     echo "      </div>"
-    echo "      <div id=\"footer\">"
-    echo "          <p><b>Versão: </b>$release_name</p>"
-    echo "      </div>"
+    echo "      <div id=\"footer\">Versão: $release_name</div>"
     echo '  </body>'
     echo '</html>'
 
